824e8dd005271a2477c1d9e4dd2c144b684d91e3
Josh Arnold <rncodepoet@gmail.com>
2023-02-09 11:03:21 -0600

    Got it!

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,83 @@
-// import { createScriptTransformer } from './transform';
+import chalk from 'chalk';
 import fs from 'fs';
 import path from 'path';
 import * as ts from 'typescript';
 import { IConfiguredRoute } from '@aesop-fables/triginta/lib/IConfiguredRoute';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import http from 'http';
-import { invokeHttpHandler } from '@aesop-fables/triginta';
+import { invokeHttpHandler, TrigintaConfig } from '@aesop-fables/triginta';
 import { IServiceContainer } from '@aesop-fables/containr';
 
 
 const CWD = process.cwd();
+const TRIGINTA_CONFIG = 'triginta.config';
+const TRIGINTA_CONFIG_FILE = `./src/${TRIGINTA_CONFIG}.ts`;
+const TRIGINTA_CONFIG_PATH = path.join(CWD, TRIGINTA_CONFIG_FILE);
+const TSCONFIG__FILE = 'tsconfig.json';
+const TSCONFIG_PATH = path.join(CWD, TSCONFIG__FILE);
+const log = console.log;
+
+const { version } = require(path.join(__dirname, '../package.json')) as { version: string };
+
+async function invokeAndAdaptHandler(req: Request, res: Response, configuredRoute: IConfiguredRoute, container: IServiceContainer): Promise<void> {
+  const response = await invokeHttpHandler({
+      configuredRoute,
+      container,
+      path: req.path,
+      body: req.body,
+  });
+
+  if (!response.body) {
+    res.sendStatus(response.statusCode);
+    return;
+  }
+
+  res.status(response.statusCode);
+  res.send(response.body);
+}
 
 module.exports.cli = async function cli(args: string[]) {
-    // const transformer = await createScriptTransformer({
-    //     cache: true,
-    //     cacheDirectory: './triginta',
-    //     haste: {},
-    //     moduleFileExtensions: [],
-    //     rootDir: './',
-    //     transform: [['ts-jest', 'ts-jest', {
-    //         '^.+\\.(t|j)s$': 'ts-jest',
-    //     }]],
-    //     transformIgnorePatterns: [],
-    //     watchPathIgnorePatterns: [],
-    // });
+    log(chalk.white.bold(`triginta ${version}`));
 
-    // await transformer.loadTransformers();
+    // Verify triginta config file exists
+    if (!fs.existsSync(TRIGINTA_CONFIG_PATH)) {
+        log(chalk.red(`Error `, chalk.white(`${TRIGINTA_CONFIG_PATH} not found.`)));
+        // throw new Error('Please create a triginta configuration file.')
+    }
 
-    // const shouldTransform = transformer.shouldTransform('./CreateTaskEndpoint.ts');
-    // console.log({
-    //   shouldTransform,
-    // });
+    let compilerOptions: ts.CompilerOptions;
+    if (fs.existsSync(TSCONFIG_PATH)) {
+        log(chalk.cyan(`info `, chalk.white(`Using tsconfig: ${TSCONFIG_PATH}`)));
+        compilerOptions = require(TSCONFIG_PATH).compilerOptions as ts.CompilerOptions;
+    } else {
+        log(chalk.cyan(`info `, chalk.white(`Using default TS compiler options. Consider creating a tsconfig.json file instead`)));
+        compilerOptions = {
+            "target": ts.ScriptTarget.ES2016 /* Set the JavaScript language version for emitted JavaScript and include compatible library declarations. */,
+            "lib": ["DOM", "ESNext"],                                        /* Specify a set of bundled library declaration files that describe the target runtime environment. */
+            "experimentalDecorators": true,                   /* Enable experimental support for TC39 stage 2 draft decorators. */
+            "emitDecoratorMetadata": true,                    /* Emit design-type metadata for decorated declarations in source files. */
+            "module": ts.ModuleKind.CommonJS /* Specify what module code is generated. */,
+            "rootDir": "src" /* Specify the root folder within your source files. */,
+            "declaration": true /* Generate .d.ts files from TypeScript and JavaScript files in your project. */,
+            "outDir": "lib" /* Specify an output folder for all emitted files. */,
+            "forceConsistentCasingInFileNames": true /* Ensure that casing is correct in imports. */,
+            "strict": true /* Enable all strict type-checking options. */,
+            "skipLibCheck": true /* Skip type checking all .d.ts files. */
+        };
+    }
 
-    // const result = await transformer.transformAsync('./CreateTaskEndpoint.ts', {
-    //     collectCoverage: false,
-    //     collectCoverageFrom: ['./CreateTaskEndpoint.ts'],
-    //     coverageProvider: 'v8',
-    //     supportsDynamicImport: false,
-    //     supportsExportNamespaceFrom: false,
-    //     supportsStaticESM: false,
-    //     supportsTopLevelAwait: false,
-    //     isInternalModule: false,
-    // });
-
-    // console.log(result.code);
-    // const compiler = new TypeScriptTestFileCompiler();
-    
-    
-    // compiler.compile(code, './src/CreateTaskEndpoint.ts');
-    const filename = "./src/index.ts";
-    const code = fs.readFileSync(filename, 'utf-8');
-    // const code = `const test: number = 1 + 2;`;
+    // Compile directory
+    const code = fs.readFileSync(TRIGINTA_CONFIG_PATH, 'utf-8');
 
     const sourceFile = ts.createSourceFile(
-        filename, code, ts.ScriptTarget.Latest
+      TRIGINTA_CONFIG_FILE, code, ts.ScriptTarget.Latest
     );
 
     const defaultCompilerHost = ts.createCompilerHost({});
 
     const customCompilerHost: ts.CompilerHost = {
         getSourceFile: (name, languageVersion) => {
-            console.log(`getSourceFile ${name}`);
-
-            if (name === filename) {
+            if (name === TRIGINTA_CONFIG_FILE) {
                 return sourceFile;
             } else {
                 return defaultCompilerHost.getSourceFile(
@@ -71,7 +86,6 @@ module.exports.cli = async function cli(args: string[]) {
             }
         },
         writeFile: (filename, data) => {
-            console.log('writing file...', filename);
             fs.writeFileSync(filename, data, 'utf-8');
         },
         getDefaultLibFileName: () => "lib.d.ts",
@@ -84,65 +98,30 @@ module.exports.cli = async function cli(args: string[]) {
         readFile: () => ""
     };
 
-    const opts = {
-        "target": ts.ScriptTarget.ES2016 /* Set the JavaScript language version for emitted JavaScript and include compatible library declarations. */,
-        "lib": ["DOM", "ESNext"],                                        /* Specify a set of bundled library declaration files that describe the target runtime environment. */
-        "experimentalDecorators": true,                   /* Enable experimental support for TC39 stage 2 draft decorators. */
-        "emitDecoratorMetadata": true,                    /* Emit design-type metadata for decorated declarations in source files. */
-        "module": ts.ModuleKind.CommonJS /* Specify what module code is generated. */,
-        "rootDir": "src" /* Specify the root folder within your source files. */,
-        "declaration": true /* Generate .d.ts files from TypeScript and JavaScript files in your project. */,
-        "outDir": "lib" /* Specify an output folder for all emitted files. */,
-        "forceConsistentCasingInFileNames": true /* Ensure that casing is correct in imports. */,
-        "strict": true /* Enable all strict type-checking options. */,
-        "skipLibCheck": true /* Skip type checking all .d.ts files. */
-      };
     const program = ts.createProgram(
-        [filename], opts, customCompilerHost
+        [TRIGINTA_CONFIG_FILE], compilerOptions, customCompilerHost
     );
 
-    if (!fs.existsSync(opts.outDir)) {
-        fs.mkdirSync(opts.outDir);
+    const outDir = compilerOptions.outDir ? path.join(CWD, compilerOptions.outDir) : path.join(CWD, './lib');
+    if (!fs.existsSync(outDir)) {
+        fs.mkdirSync(outDir);
     }
 
-    // NOTE: The first argument of emit() is a source file to be compiled. If it's undefined, all files in
-    // <program> will be compiled. <program> contains a file specified in createProgram() plus all its dependencies.
-    // This mode is much faster than compiling files one-by-one, and it is used in the tsc CLI compiler.
     program.emit();
 
-    // Now that it's compiled, let's evaluate all of the files
-    const { container: rawContainer, RouteRegistry } = require(path.join(CWD, './lib/index.js'));
-    const container = rawContainer as IServiceContainer;
-    // const { endpoint } = require(path.join(CWD, './lib/CreateTaskEndpoint.js'));
+    log(chalk.green(`success `, chalk.white(`Compiled typescript to ${outDir}`)));
 
-    // console.log(handler);
-    // try {
-    //     console.log('attempting to warm up...');
-    //     handler({});
-    // } catch (e) {
-    //     console.log('Error warming up!', e);
-    // }
+    // Retrieve config from triginta config
+    const { container, routes: routeRegistry } = require(path.join(outDir, `${TRIGINTA_CONFIG}.js`)) as TrigintaConfig;
 
-    // console.log(RouteRegistry.allRoutes());
-
-    // console.log(RouteRegistry.allRoutes());
-
-    const routes = RouteRegistry.allRoutes() as IConfiguredRoute[];
-    console.log(routes);
-    for (let i = 0; i < routes.length; i++) {
-        const configuredRoute = routes[i];
-        console.log(`Found configuredRoute: ${configuredRoute.method} ${configuredRoute.route}`);
-    }
-
+    // TODO -- Expose the port in triginta config
     const options = {
-    port: 3002,
-    ...opts,
+      port: 3002,
     };
 
+    // Create server
     const app = express();
     app.use(express.json());
-
-    // app.options('*', cors());
 
     app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
@@ -153,40 +132,28 @@ module.exports.cli = async function cli(args: string[]) {
         next();
     });
 
+    const routes = routeRegistry.allRoutes() as IConfiguredRoute[];
+    log(chalk.white(`Evaluating routes`));
     for (let i = 0; i < routes.length; i++) {
         const configuredRoute = routes[i];
-        if (configuredRoute.method === 'post') {
-            app.post(configuredRoute.route, async (req, res) => {
-                const response = await invokeHttpHandler({
-                    configuredRoute,
-                    container,
-                    path: req.path,
-                    body: req.body,
-                });
-
-                console.log('GOT SOMETHING!', response);
-
-                res.sendStatus(200);
-            });
-        }
-
-        if (configuredRoute.method === 'get') {
-            app.get(configuredRoute.route, async (req, res) => {
-                const response = await invokeHttpHandler({
-                    configuredRoute,
-                    container,
-                    path: req.path,
-                    body: req.body,
-                });
-
-                console.log('GOT SOMETHING!', response);
-
-                res.sendStatus(200);
-            });
+        log(chalk.gray(`  - Configured ${configuredRoute.method} ${configuredRoute.route}`));
+        switch (configuredRoute.method.toLowerCase()) {
+          case 'get':
+            app.get(configuredRoute.route, async (req, res) => invokeAndAdaptHandler(req, res, configuredRoute, container));
+            break;
+          case 'post':
+            app.post(configuredRoute.route, async (req, res) => invokeAndAdaptHandler(req, res, configuredRoute, container));
+            break;
+          case 'put':
+            app.put(configuredRoute.route, async (req, res) => invokeAndAdaptHandler(req, res, configuredRoute, container));
+            break;
+          case 'delete':
+            app.delete(configuredRoute.route, async (req, res) => invokeAndAdaptHandler(req, res, configuredRoute, container));
+            break;
         }
     }
 
-    console.log(`triginta listening at http://localhost:${options.port}`)
+    log(chalk.white(`triginta listening on http://localhost:${options.port}`));
     const server = http.createServer(app);
     server.listen(options.port);
 }
